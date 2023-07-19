c730f6525474e8c1181bca7f127d20e68b1bf54a
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-07-19 21:30:10 +0530

    Create IntersectionOf2LinkedList.java

---
+++ IntersectionOf2LinkedList.java
@@ -0,0 +1,17 @@
+public class IntersectionOf2LinkedList {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a=headA;
+        ListNode b=headB;
+        while(a!=b){
+            if(a==null){
+                a=headB;
+            }
+            else
+            a=a.next;
+            if(b==null)
+            b=headA;
+            else b=b.next;
+        }
+        return a;
+    }
+}
