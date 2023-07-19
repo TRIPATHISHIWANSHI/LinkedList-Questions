6b4f1897fee29b3c6a9e63407f1d882476a176c2
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-07-19 21:47:47 +0530

    Create Reverse_Linked_List.java

---
+++ Reverse_Linked_List.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
